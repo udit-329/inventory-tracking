ecf32c2004159755d5075b6745a2888ee68e3bd4
Udit Kapoor <ukapoor.29@gmail.com>
2022-01-11 16:54:02 -0500

    added all routes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -8,6 +8,9 @@ import (
 
 var RegisterRoutes = func(router *mux.Router) {
 	router.HandleFunc("/add/", controllers.AddItem).Methods("POST")
-	router.HandleFunc("/update/{SKU}", controllers.AddItem).Methods("POST") //PUT
-	router.HandleFunc("/delete/{SKU}", controllers.AddItem).Methods("DELETE")
+	router.HandleFunc("/get/{id}", controllers.GetItemById).Methods("GET")
+	router.HandleFunc("/get/", controllers.GetItems).Methods("GET")
+	router.HandleFunc("/update/{SKU}", controllers.UpdateItem).Methods("PUT")
+	router.HandleFunc("/delete/{SKU}", controllers.DeleteItem).Methods("DELETE")
+	router.HandleFunc("/export/", controllers.ExportItems).Methods("GET")
 }
